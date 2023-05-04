46f54acc2c1ce4a3e04be6f25dd45219f5a08c95
Nikita Trenikhin <nik.trenikhin@gmail.com>
2023-05-04 05:23:25 +0500

    no message

--- Assets/Scripts/Other/UnitPool.cs
+++ Assets/Scripts/Other/UnitPool.cs
@@ -23,6 +23,7 @@ public class UnitPool : MonoBehaviour
         if (other.TryGetComponent(out PlayerInfo player))
         {
             Remove(player);
+        }
     }
 
     public void Add(PlayerInfo player)
